44ebc384903fddfeb293d6d1a527998c74e96f16
Nda-cunh <hydraldev@gmail.com>
2024-05-09 06:26:31 +0200

    Add sfml (not google)

--- sfml/main.cpp
+++ sfml/main.cpp
@@ -4,22 +4,56 @@
 #include <SFML/Window.hpp>
 #include "../include/IPlugin.hpp"
 #include <memory>
+#include <cmath>
 
 
 class Plugin : public IPlugin {
 	private:
 		sf::RenderWindow *window;
+		sf::Sprite Sdamier;
+		sf::RenderTexture Tdamier;
+		sf::Texture Tapple;
+		sf::Sprite Sapple;
+		int tileX;
+		int tileY;
 	public:
-		~Plugin () {
-		}
-		Plugin () {
+		void open(int x, int y){
+			window = new sf::RenderWindow(sf::VideoMode(800, 800), "Hello SFML");
+			window->setFramerateLimit(60);
+			tileX = 800 /x;
+			tileY = 800 /y;
+			init_background();
+			draw_background();
+			
+			Tapple.loadFromFile("/nfs/homes/nda-cunh/Desktop/nibbler/sfml/food.png");
+			Sapple.setTexture(Tapple);
 		}
 
-		void open(int x, int y){
-			window = new sf::RenderWindow(sf::VideoMode(500, 500), "Hello SFML");
+		void init_background() {
+			Tdamier.create(800, 800);
+			float width = tileX;
+			float height = tileY;
+			int num_tiles_x = window->getSize().x / width;
+			int num_tiles_y = window->getSize().y / height;
+
+			sf::RectangleShape rectangle({width, height});
+			for (int i = 0; i < num_tiles_x; ++i) {
+				for (int j = 0; j < num_tiles_y; ++j) {
+					if ((i + j) % 2 == 0) {
+						rectangle.setFillColor(sf::Color(162, 209, 73)); // couleur de rectangle1
+					} else {
+						rectangle.setFillColor(sf::Color(170, 215, 81)); // couleur de rectangle2
+					}
+					rectangle.setPosition(i * width, j * height);
+					Tdamier.draw(rectangle);
+				}
+			}
+			Tdamier.display();
+			Sdamier.setTexture(Tdamier.getTexture());
 		}
 
 		void close(){
+			window->close();
 			delete window;
 		}
 		
@@ -39,20 +73,48 @@ class Plugin : public IPlugin {
 				return CLOSE;
 			return NONE;
 		}
+	
+		void draw_segment(const Position begin, const Position end, double size) {
+			sf::CircleShape circle(size);
+			circle.setPointCount(7);
+			circle.setFillColor(sf::Color(78, 125, 246, 255));
+			double diff = (tileX / 2.0) - size;
 
-		// void rect(int x, int y){
-			// auto rec = sf::RectangleShape(sf::Vector2f(50, 50));
-			// rec.setPosition(x, y);
-			// window->draw(rec);
-		// }
-		
-		
-		void draw_snake(std::deque<Position>) {
+			Position b = {begin.x * tileX, begin.y * tileY};
+			Position e = {end.x * tileX, end.y * tileY};
+			while (b.x != e.x) {
+				circle.setPosition(b.x + diff, b.y + diff);
+				window->draw(circle);
+				if (e.x > b.x)
+					b.x++;
+				else
+					b.x--;
+			}
+			while (b.y != e.y) {
+				circle.setPosition(b.x + diff, b.y + diff);
+				window->draw(circle);
+				if (e.y > b.y)
+					b.y++;
+				else
+					b.y--;
+			}
+		}
 
+		void draw_snake(std::deque<Position> snake) {
+
+			Position last = snake[0];
+			double size = tileX / 2.0;
+			for (auto i : snake) {
+				draw_segment(i, last, size);
+				last = i;
+				size -= 0.2;
+			}
 		}
 
 		void draw_food(Position &position) {
 
+			Sapple.setPosition(position.x * tileX, position.y * tileY);
+			window->draw(Sapple);
 		}
 
 		void draw_score(int n) {
@@ -63,9 +125,15 @@ class Plugin : public IPlugin {
 
 		}
 
+
+		void draw_background() {
+			window->draw(Sdamier);
+		}
+
 		void iteration () {
 			window->display();
 			window->clear();
+			draw_background();
 		}
 };
 
