c4d82045ad14d36eb9d4ebc7b93a0d33ca06232b
Thibo_M <thibaud.marin@proton.me>
2024-05-13 01:44:33 +0200

    Refacto: Game class
    - Improve generate food method (avoid infinite loop on full map)
    - Add cpp file associated with header file

---
+++ core/Game.cpp
@@ -0,0 +1,80 @@
+#include "Game.hpp"
+
+/* ____ CONSTRUCTORS ____ */
+Game::Game()	{}
+
+Game::~Game()	{}
+
+Game::Game(const int width, const int height) {
+	_snake.create(width, height);
+	_size = {width, height};
+	_is_over = false;
+	_score = 0;
+	_foods = std::vector<Position>();
+	for (int i = 0; i < width*height; ++i)
+		generateFood();
+}
+
+/* ____ ACCESSORS ____ */
+bool	Game::over() const {
+	return _is_over;
+}
+
+int		Game::getScore() const {
+	return _score;
+}
+
+const std::deque<Position> &Game::getSnakePositions( void ) const {
+	return _snake.getPositions(); 
+}
+
+const std::vector<Position> &Game::getFoodPositions( void ) const {
+	return _foods;
+}
+
+/* ____ METHODS ____ */
+void Game::moveSnake(const Direction &dir) {
+	const Position new_head_pos = _snake.move(dir);
+	const auto		snake_pos = _snake.getPositions();
+	const auto		food_it = FIND(_foods, new_head_pos);
+
+	// Check for any lose
+	if (new_head_pos.x >= _size.x || new_head_pos.x < 0)
+		_is_over = true;
+	else if (new_head_pos.y >= _size.y || new_head_pos.y < 0)
+		_is_over = true;
+	else if (COUNT(_snake.getPositions(), new_head_pos) > 1)
+		_is_over = true;
+	else if (food_it == _foods.end())
+		_snake.loseTail();
+	else {
+		_score++;
+		_foods.erase(food_it);
+		generateFood();
+		generateFood();
+	}
+}
+
+void	Game::generateFood() {
+	const auto 		snake_pos = getSnakePositions();
+	auto freePos = std::vector<Position>();
+
+	// Generate all available positions
+	for (int i = 0; i < _size.x; ++i) {
+		for (int j = 0; j < _size.y; ++j) {
+			if (FIND(snake_pos, Position({i, j})) != snake_pos.end())
+				continue;
+			else if (FIND(_foods, Position({i, j})) != _foods.end())
+				continue;
+			freePos.push_back({i, j});
+		}
+	}
+
+	// No empty spaces in map
+	if (freePos.size() == 0)
+		return ;
+
+	int rand_idx =  std::rand() % freePos.size();
+	_foods.push_back(freePos[rand_idx]);
+}
+
