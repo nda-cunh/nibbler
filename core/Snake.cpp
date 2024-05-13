09b9c2ef931814b874ed23aef6e401fcff3aa02f
Thibo_M <thibaud.marin@proton.me>
2024-05-13 02:26:45 +0200

    Refacto: Timer & Snake class
    - Added cpp files according classes
    - Added cpp files in Makefile

---
+++ core/Snake.cpp
@@ -0,0 +1,50 @@
+# include "Snake.hpp"
+
+/* ____ CONSTRUCTORS ____ */
+Snake::Snake() {}
+Snake::~Snake() {}
+
+void Snake::create(const int &width, const int &height) {
+	for (int j = 4; j > 0; j--)
+		_positions.push_front({width / 2, height / 2 - j});
+	_dir = Down;
+}
+
+
+/* ____ ACCESSORS ____ */
+const std::deque<Position>	&Snake::getPositions( void )  const { return _positions; }
+
+
+/* ____ METHODS ____ */
+void	Snake::loseTail( void ) { _positions.pop_back(); }
+
+const Position	Snake::move(const Direction &new_dir) {
+	if (this->isCompatibleDir(new_dir))
+		_dir = new_dir;
+	Position	head_pos = _positions[0];
+
+	switch (_dir) {
+		case Left:
+			head_pos = {head_pos.x - 1, head_pos.y};
+			break;
+		case Right:
+			head_pos = {head_pos.x + 1, head_pos.y};
+			break;
+		case Up:
+			head_pos = {head_pos.x, head_pos.y - 1};
+			break;
+		case Down:
+			head_pos = {head_pos.x, head_pos.y + 1};
+			break;
+	}
+	_positions.push_front(head_pos);
+	return head_pos;
+}
+
+bool	Snake::isCompatibleDir(const Direction &e) const {
+	if (_dir == Up || _dir == Down)
+		return (e == Left || e == Right);
+	else if (_dir == Left  || _dir == Right)
+		return (e == Up || e == Down);
+	return false;
+}
