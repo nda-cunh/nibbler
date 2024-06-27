314038bcd257f5b29cc164baf0d249345d8ae5e7
Thibo_M <thibaud.marin@proton.me>
2024-06-27 05:35:27 +0200

    First Try: Interface changes

--- include/IPlugin.hpp
+++ include/IPlugin.hpp
@@ -32,11 +32,11 @@ class IPlugin {
 		virtual ~IPlugin() = default;
 		virtual void open(int, int) = 0;
 		virtual void close() = 0;
-		virtual void update_snake(const std::deque<Position> &snake, Direction direction) = 0;
+		virtual void update_snake(const std::deque<Position> &p1, const std::deque<Position> &p2 = {}) = 0;
 		virtual void update_food(Position &position) = 0;
-		virtual void update_score(int n) = 0;
+		virtual void update_score(int score_p1, int score_p2 = 0) = 0;
 		virtual void update_speed(int n) = 0;
-		virtual void update_bestscore(int n) = 0;
+		virtual void update_bestscore(int best_score_p1, int best_score_p2 = 0) = 0;
 		virtual void clear () = 0;
 		virtual void display (const Activity) = 0;
 		virtual Event poll_event(const Activity) = 0;
