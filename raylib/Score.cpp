0c282034a91d719aff4373fa1de7d83ba948a37b
Thibo_M <thibaud.marin@proton.me>
2024-10-16 23:18:09 +0200

    Update: Score class
    - Added canonical form
    - Delete unused variables
    - Add comments

--- raylib/Score.cpp
+++ raylib/Score.cpp
@@ -1,14 +1,51 @@
-#include "Score.hpp"
+# include "Score.hpp"
 # include <string>
 
-Score::Score(Color c, Vector2 pos) {
-	this->_color = c;
-	this->_position = pos;
+/* ____ CONSTRUCTORS/ DESTRUCTOR ____ */
+
+Score::Score(): _color(WHITE), _position({0, 0}) {}
+
+Score::Score(Color c, Vector2 pos): _color(c), _position(pos) {}
+
+Score::Score(const Score &other) {
+	*this = other;
 }
 
+Score::~Score() {}
+
+/* ____ ASSIGNEMENT OPERATOR ____ */
+Score &Score::operator=(const Score &rhs) {
+	if (this == &rhs)
+		return *this;
+
+	this->_color = rhs._color;
+	this->_position = rhs._position;
+	return *this;
+}
+
+/* ____ PUBLIC METHODS ____ */
+void Score::draw(int n) const {
+	int decimal = n % 10;
+	int power = 1;
+
+	if (n == 0)
+		this->drawDigit(0, {_position.x - 30 * power, _position.y});
+	while (n > 0) {
+		this->drawDigit(decimal, {_position.x - 30 * power, _position.y});
+		n /= 10;
+		decimal = n % 10;
+		power++;
+	}
+	while (power < 4) {
+		this->drawDigit(0, {_position.x - 30 * power, _position.y});
+		power++;
+	}
+}
+
+/* ____ PRIVATE DRAW METHODS ____ */
 void Score::drawLed(LedPosition led_pos, Vector2 start, Color c) const {
-	float rdns = 0.8f;
-	Vector2	shape = {15, 3};
+	const float rdns = 0.8f;
+	const Vector2	shape = {15, 3};
 
 	switch (led_pos) {
 		case Top:
@@ -45,15 +82,15 @@ void Score::drawLed(LedPosition led_pos, Vector2 start, Color c) const {
 			DrawRectangleRounded({start.x, start.y + 4 * shape.y + 2 * shape.x,
 					shape.x, shape.y}, rdns, 10, c);
 			break;
+		default:
+			break;
 	}
 }
 
 void Score::drawBackground(Vector2 start) const {
 	Color c = this->_color;
-	float rdns = 0.8f;
-	Vector2	shape = {15, 3};
-
 	c.a = 30;
+
 	drawLed(Top, start, c);
 	drawLed(TopLeft, start, c);
 	drawLed(TopRight, start, c);
@@ -138,20 +175,3 @@ void Score::drawDigit(int digit, Vector2 start) const {
 	}
 }
 
-void Score::draw(int n) const {
-	int decimal = n % 10;
-	int power = 1;
-
-	if (n == 0)
-		this->drawDigit(0, {_position.x - 30 * power, _position.y});
-	while (n > 0) {
-		this->drawDigit(decimal, {_position.x - 30 * power, _position.y});
-		n /= 10;
-		decimal = n % 10;
-		power++;
-	}
-	while (power < 4) {
-		this->drawDigit(0, {_position.x - 30 * power, _position.y});
-		power++;
-	}
-}
--- raylib/Score.hpp
+++ raylib/Score.hpp
@@ -15,16 +15,23 @@ typedef enum LedPosition {
 
 class Score {
 	public:
+		/* ---- Constructors/ Destructor ---- */
+		Score();
+		Score(const Score &other);
 		Score(Color c, Vector2 pos);
+		~Score();
 
+		/* ---- Assignement operator ---- */
+		Score &operator=(const Score &rhs);
+
+		/* ---- Public methods ---- */
 		void draw(int n) const;
-
-
 
 	private:
 		Color 	_color;
 		Vector2 _position;
 
+		/* ---- Private draw methods ---- */
 		void drawDigit(int digit, Vector2 start) const; 
 		void drawLed(LedPosition led_pos, Vector2 start, Color c) const; 
 		void drawBackground(Vector2 start) const; 
